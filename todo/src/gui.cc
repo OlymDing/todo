9d6e0ee8a46d4aa4947dada883d13531be7cd0e5
olymding <olymding@tencent.com>
2026-02-09 21:30:00 +0800

    basic card compnents added

--- todo/src/gui.cc
+++ todo/src/gui.cc
@@ -5,11 +5,11 @@ GUI::GUI()
 {
   PainterEngine_Initialize(800, 480);
   PainterEngine_LoadFontModule(
-      "assets/font.ttf", PX_FONTMODULE_CODEPAGE_GBK, 20
+      "/home/olym/Downloads/CascadiaMono/CaskaydiaMonoNerdFont-Bold.ttf", PX_FONTMODULE_CODEPAGE_GBK, 20
   );
 }
 
 void GUI::loop()
 {
-  PX_Object *card = PX_Object_TodoCardCreate(mp, root, 128, 128);
+  PX_Object *card = PX_Object_TodoCardCreate(mp, root, 0, 0);
 }
--- todo/src/todo_gui_card/card.cc
+++ todo/src/todo_gui_card/card.cc
@@ -1,5 +1,7 @@
 #include <PainterEngine.h>
+#include "core/PX_Typedef.h"
 #include "gui.hpp"
+#include "kernel/PX_Object_Label.h"
 
 PX_OBJECT_EVENT_FUNCTION(OnButtonClick)
 {
@@ -8,7 +10,8 @@ PX_OBJECT_EVENT_FUNCTION(OnButtonClick)
 
 typedef struct
 {
-  PX_Object *left_button, *right_button;
+  PX_Object *title_header, *title_text;
+  PX_Object *status_header, *status_text;
 } TodoCard;
 
 PX_OBJECT_UPDATE_FUNCTION(TodoCardUpdate)
@@ -17,9 +20,15 @@ PX_OBJECT_UPDATE_FUNCTION(TodoCardUpdate)
 }
 PX_OBJECT_RENDER_FUNCTION(TodoCardRender)
 {
-  TodoCard *card = PX_ObjectGetDesc(TodoCard, pObject);
-  PX_ObjectRender(psurface, card->left_button, 0);
-  PX_ObjectRender(psurface, card->right_button, 0);
+
+  px_rect rect = PX_ObjectGetRect(pObject);
+  auto x = (px_int)rect.x;
+  auto y = (px_int)rect.y;
+  auto w = (px_int)rect.width;
+  auto h = (px_int)rect.height;
+
+  PX_GeoDrawBorder(psurface, x, y, x + w, y + h - 1, 1, PX_COLOR_BLACK);
+  // TodoCard *card = PX_ObjectGetDesc(TodoCard, pObject);
   // todo
 }
 PX_OBJECT_FREE_FUNCTION(TodoCardFree)
@@ -32,23 +41,41 @@ PX_Object *PX_Object_TodoCardCreate(
 )
 {
   PX_Object *pObject = PX_ObjectCreateEx(
-      mp, parent, x, y, 0, 128, 128, 0, 0, TodoCardUpdate, TodoCardRender,
+      mp, parent, x, y, 0, 220, 100, 0, 0, TodoCardUpdate, TodoCardRender,
       TodoCardFree, 0, sizeof(TodoCard)
   );
   TodoCard *card = PX_ObjectGetDescIndex(TodoCard, pObject, 0);
-  card->left_button = PX_Object_PushButtonCreate(
-      mp, pObject, 300, 200, 200, 40, "hello left",
-      PainterEngine_GetFontModule()
+
+  // card->left_button = PX_Object_PushButtonCreate(
+  //     mp, pObject, 100, 0, 200, 40, "hello left",
+  //     PainterEngine_GetFontModule()
+  // );
+  // card->right_button = PX_Object_PushButtonCreate(
+  //     mp, pObject, 100, 39, 200, 40, "hello right",
+  //     PainterEngine_GetFontModule()
+  // );
+  card->title_header = PX_Object_LabelCreate(
+      mp, root, x + 10, y + 10, 90, 40,
+      "title: ", PainterEngine_GetFontModule(), PX_COLOR_BLACK
   );
-  card->right_button = PX_Object_PushButtonCreate(
-      mp, pObject, 300, 239, 200, 40, "hello right",
-      PainterEngine_GetFontModule()
+  // PX_Object_LabelSetBorder(card->title_header, true);
+
+  card->title_text = PX_Object_LabelCreate(
+      mp, root, x + 110, y + 10, 100, 40, "title text",
+      PainterEngine_GetFontModule(), PX_COLOR_BLACK
   );
-  PX_ObjectRegisterEvent(
-      card->right_button, PX_OBJECT_EVENT_EXECUTE, OnButtonClick, 0
+  // PX_Object_LabelSetBorder(card->title_text, true);
+
+  card->status_header = PX_Object_LabelCreate(
+      mp, root, x + 10, y + 50, 90, 40,
+      "status: ", PainterEngine_GetFontModule(), PX_COLOR_BLACK
   );
-  PX_ObjectRegisterEvent(
-      card->left_button, PX_OBJECT_EVENT_EXECUTE, OnButtonClick, 0
+  // PX_Object_LabelSetBorder(card->status_header, true);
+
+  card->status_text = PX_Object_LabelCreate(
+      mp, root, x + 110, y + 50, 100, 40, "status text",
+      PainterEngine_GetFontModule(), PX_COLOR_BLACK
   );
+  // PX_Object_LabelSetBorder(card->status_text, true);
   return pObject;
 }
